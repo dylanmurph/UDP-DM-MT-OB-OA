504d3be4fa9f60dfee0155cab5d744432729a55e
Matthew Tomkins <mattjpt@gmail.com>
2025-11-06 18:25:48 +0000

    Add NFC tag detection test code

---
+++ nfc_test.py
@@ -0,0 +1,14 @@
+import board
+import busio
+from adafruit_pn532.i2c import PN532_I2C
+
+i2c = busio.I2C(board.SCL, board.SDA)
+pn532 = PN532_I2C(i2c, debug=False)
+pn532.SAM_configuration()
+
+print("Waiting for NFC tag...")
+
+while True:
+    uid = pn532.read_passive_target(timeout=0.5)
+    if uid is not None:
+        print("Detected UID:", [hex(i) for i in uid])
