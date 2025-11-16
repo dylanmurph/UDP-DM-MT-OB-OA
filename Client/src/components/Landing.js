c48e7b2293ec86fc3d38b006c7c03874f6566c4d
lareishim <161841580+oluwadamilare-adekeye@users.noreply.github.com>
2025-11-16 01:16:12 +0000

    Chore: fixed file structure

--- Client/src/components/Landing.js
+++ Client/src/components/Landing.js
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 function Landing() {
   return (
     <div>
-      <h1>Welcome to BnB Smart Access</h1>
+      <h1>Welcome to HostLock</h1>
       <p>Please choose an option:</p>
       <div style={{ display: "flex", gap: "1rem", justifyContent: "center" }}>
         <Link to="/register">
@@ -13,9 +13,6 @@ function Landing() {
         <Link to="/login">
           <button>Login</button>
         </Link>
-        <div className="text-center bg-gray-200 p-6 rounded-lg shadow">
-          <h1 className="text-3xl font-bold text-blue-600">Hello Tailwind!</h1>
-        </div>
       </div>
     </div>
   );
