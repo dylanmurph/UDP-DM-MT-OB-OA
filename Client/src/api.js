9cd9941095d8b63ca9cc8fe99c6430153f0c2333
osh-een <158780048+osh-een@users.noreply.github.com>
2025-11-19 15:20:04 +0000

    Refactor: Update interceptors in api file

--- Client/src/api.js
+++ Client/src/api.js
@@ -4,9 +4,15 @@ const api = axios.create({
   baseURL: 'https://www.hostlocksd3b.online/api'
 });
 
-api.interceptors.request.use(config => {
+api.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
-  if (token) config.headers.Authorization = `Bearer ${token}`;
+
+  if (token && token.length > 20) {
+    config.headers.Authorization = `Bearer ${token}`;
+  } else {
+    delete config.headers.Authorization;
+  }
+
   return config;
 });
 
